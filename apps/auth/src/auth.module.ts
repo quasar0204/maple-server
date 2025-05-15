8fe45397ba28af839a8264d89c84a3eac59ff6a5
박준성 <junseong@bagjunseong-ui-MacBookAir.local>
2025-05-15 23:31:00 +0900

    mongooseMoudle 추가

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
